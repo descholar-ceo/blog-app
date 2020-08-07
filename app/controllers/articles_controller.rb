18d4033344a7cb59130c0d7b32e94e5d8867abf5
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-07 12:41:01 +0200

    Add create and new actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    
+  end
 end
