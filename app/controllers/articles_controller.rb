caaab3b49cdba9dad5aaf71760b7b0cf6e078591
Georjane <pzgeorjane@gmail.com>
2020-08-07 12:39:29 +0100

    Add edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,16 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    @article =Article.find(params[:id])
+  end
+
+  def update
+    @article =Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
