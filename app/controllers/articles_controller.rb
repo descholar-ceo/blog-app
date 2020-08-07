cfef86d70f4df531a0308f040c0a3fd4b7254b87
Georjane <pzgeorjane@gmail.com>
2020-08-07 12:04:53 +0100

    Add create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,5 +13,10 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.author = params[:article][:author]
+    @article.save
+    redirect_to article_path(@article)
   end
 end
