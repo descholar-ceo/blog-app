a8742e7003a4d167c1441b455fd298440155481f
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-07 17:48:34 +0200

    Add homework answers

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,4 +5,9 @@ class TagsController < ApplicationController
     def show
         @tag = Tag.find(params[:id])
     end
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        redirect_to tags_path
+    end
 end
