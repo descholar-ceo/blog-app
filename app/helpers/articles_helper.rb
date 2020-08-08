4792dda105497de1cd1335fe7013d2d68aa05c6d
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-08 10:57:33 +0200

    Integrate paperclip in the app's codes

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
   def article_params
-    params.require(:article).permit(:title, :body, :author, :tag_list)
+    params.require(:article).permit(:title, :body, :author, :tag_list, :image)
   end
 end
