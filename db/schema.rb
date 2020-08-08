c47ac0c31f57f7d86d3892b50ee232f67a7e4622
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-08 10:53:08 +0200

    Add paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_142833) do
+ActiveRecord::Schema.define(version: 2020_08_08_084827) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2020_08_07_142833) do
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
