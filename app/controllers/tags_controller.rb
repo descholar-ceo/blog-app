acbefe1ad28bb6c2bde3e69500d0a66299e16232
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-08 10:38:31 +0200

    Finish tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,6 +8,7 @@ class TagsController < ApplicationController
     def destroy
         @tag = Tag.find(params[:id])
         @tag.destroy
+        flash.notice = "This tag #{@tag.name} has been deleted successufully!"
         redirect_to tags_path
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     has_many :comments
-    has_many :taggings
+    has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
 
     def tag_list
